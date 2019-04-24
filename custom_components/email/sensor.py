6cc49be6cc575c643e91db12510a5bd3af764258
Leonardo Merza <ljmerza@gmail.com>
2019-04-24 12:00:00 -0400

    added rockauto; use imapclient library

--- custom_components/email/sensor.py
+++ custom_components/email/sensor.py
@@ -2,9 +2,7 @@
 from datetime import timedelta
 import imaplib
 import email
-import json
 import logging
-import smtplib
 
 import voluptuous as vol
 
@@ -22,6 +20,8 @@ CONF_SMTP_SERVER = 'smtp_server'
 CONF_SMTP_PORT = 'smpt_port'
 CONF_EMAIL_FOLDER = 'folder'
 
+SCAN_INTERVAL = timedelta(seconds=300)
+
 PLATFORM_SCHEMA = PLATFORM_SCHEMA.extend({
     vol.Required(CONF_EMAIL): cv.string,
     vol.Required(CONF_PASSWORD): cv.string,
@@ -30,43 +30,66 @@ PLATFORM_SCHEMA = PLATFORM_SCHEMA.extend({
     vol.Required(CONF_EMAIL_FOLDER, default='INBOX'): cv.string,
 })
 
-SCAN_INTERVAL = timedelta(seconds=300)
-
 
 def setup_platform(hass, config, add_entities, discovery_info=None):
     """Set up the Email platform."""
-    email_address = config[CONF_EMAIL]
-    password = config[CONF_PASSWORD]
+    from imapclient import IMAPClient
+    
     smtp_server = config[CONF_SMTP_SERVER]
     smtp_port = config[CONF_SMTP_PORT]
+
+    email_address = config[CONF_EMAIL]
+    password = config[CONF_PASSWORD]
     email_folder = config[CONF_EMAIL_FOLDER]
 
     try:
-        imap = imaplib.IMAP4_SSL(smtp_server)
-        imap.login(email_address, password)
-        
-        rv, data = imap.select(email_folder, readonly=True)
-        if rv != 'OK':
-            _LOGGER.error(f'IMAP4 error: {err}')
-            return False
-
-        add_entities([EmailEntity(imap, email_address)], True)
+        server = IMAPClient(smtp_server, use_uid=True)
+        server.login(email_address, password)
+        select_info = server.select_folder(email_folder, readonly=True)
+        add_entities([EmailEntity(server, config)], True)
         return True
 
-    except imaplib.IMAP4.error as err:
-        _LOGGER.error(f'IMAP4 error: {err}')
+    except Exception as err:
+        _LOGGER.error(f'IMAPClient setup_platform error: {err}')
         return False
 
 
 class EmailEntity(Entity):
     """Email Entity."""
 
-    def __init__(self, imap, email_address):
+    def __init__(self, server, config):
         """Init the Email Entity."""
-        self.imap = imap
-        self.email_address = email_address
+        self.server = server
+        self.email_address = config[CONF_EMAIL]
+
         self.emails = []
         self.email_count = 0
+
+    def update(self):
+        """Update data from Email API."""
+        self.emails = []
+        
+        try: 
+            messages = self.server.search('UNSEEN')
+            for uid, message_data in self.server.fetch(messages, 'RFC822').items():
+                try:
+                    email_message = email.message_from_bytes(message_data[b'RFC822'])
+                    try:
+                        msg = str(email_message.get_payload(0))
+                        self.emails.append({
+                            'from': email_message.get('from'),
+                            'subject': email_message.get('subject'),
+                            'msg': msg
+                        })
+                    except Exception as err:
+                        LOGGER.error(f'IMAPClient get_payload error: {err}')
+                except Exception as err:
+                    _LOGGER.error(f'IMAPClient message_from_bytes error: {err}')
+
+        except Exception as err:
+            _LOGGER.error(f'IMAPClient update error: {err}')
+
+        self.email_count = len(self.emails)
 
     @property
     def name(self):
@@ -90,37 +113,3 @@ class EmailEntity(Entity):
     def icon(self):
         """Return the icon to use in the frontend."""
         return 'mdi:email'
-
-    def update(self):
-        """Update data from Email API."""
-        self.emails = []
-        self.email_count = 0
-        
-        try: 
-            status, sdata = self.imap.search(None, 'UNSEEN')
-        except imaplib.IMAP4.error as err:
-            _LOGGER.error(f'IMAP4 search error: {err}')
-
-        mail_ids = sdata[0]
-
-        id_list = mail_ids.split()
-        self.email_count = len(id_list)
-        
-        for i in id_list:
-            try:
-                typ, data = self.imap.fetch(i, '(RFC822)')
-                if typ != 'OK':
-                    _LOGGER.error(f'IMAP4 fetch error: {data}')
-                    continue
-            except imaplib.IMAP4.error as err:
-                _LOGGER.error(f'IMAP4 fetch error: {err}')
-
-            try:
-                msg = email.message_from_string(data[0][1].decode('UTF-8'))
-                self.emails.append({
-                    'from': msg.get('from'),
-                    'subject': msg.get('subject'),
-                    'msg': str(msg.get_payload(0))
-                })
-            except TypeError as err:
-                _LOGGER.error(f'IMAP4 update error: {err}')
---
+++ node-red/rockauto-tracking-mqtt/README.md
@@ -0,0 +1,15 @@
+# RockAuto MQTT Tracking Numbers
+
+
+## Description
+
+This flow parses out tracking numbers from RockAuto emails and sends them back to Home Assistant via MQTT. You'll need `node-red-contrib-home-assistant-websocket` and `mqtt` setup in Node-RED along with a MQTT broker setup in Home Assistant. Add the flow to Node-RED and add the MQTT sensor in Home Assistant:
+
+```yaml
+sensor:
+    - platform: mqtt
+        name: RockAuto
+        state_topic: "rockauto"
+        value_template: '{{ value_json.count }}'
+        json_attributes_topic: "rockauto"
+```
