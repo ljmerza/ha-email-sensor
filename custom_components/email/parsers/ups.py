26ba7d7f9358eaf435282d27e27cdeac7ceb162a
Leonardo Merza <ljmerza@gmail.com>
2019-08-21 20:51:56 -0400

    make ups parser not so strict

--- custom_components/email/parsers/ups.py
+++ custom_components/email/parsers/ups.py
@@ -24,9 +24,10 @@ def parse_ups(email):
         links = [link.get('href') for link in soup.find_all('a')]
         for link in links:
             if not link: continue
-            match = re.search('tracknum=(.*?)&AgreeToTermsAndConditions', link)
+
+            match = re.search('tracknum=(.*?)&', link)
             if match and match.group(1) not in tracking_numbers:
                 tracking_numbers.append(match.group(1))
-                
+
     return tracking_numbers
     
