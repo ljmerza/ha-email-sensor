cda3bd777a465c15306535ceed5d1c9079c0696c
Leonardo Merza <ljmerza@gmail.com>
2023-07-03 15:12:39 -0400

    remove generic parser fedex

--- custom_components/email/parsers/generic.py
+++ custom_components/email/parsers/generic.py
@@ -24,9 +24,9 @@ def parse_generic(email):
         if tracking_number not in tracking_numbers:
             tracking_numbers.append(tracking_number)
 
-    matches = re.findall(FEDEX_TRACKING_NUMBER_REGEX, email[EMAIL_ATTR_BODY])
-    for tracking_number in matches:
-        if tracking_number not in tracking_numbers:
-            tracking_numbers.append(tracking_number)
+    # matches = re.findall(FEDEX_TRACKING_NUMBER_REGEX, email[EMAIL_ATTR_BODY])
+    # for tracking_number in matches:
+    #     if tracking_number not in tracking_numbers:
+    #         tracking_numbers.append(tracking_number)
 
     return tracking_numbers
