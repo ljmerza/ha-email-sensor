57e52a6688eb043b43a8b7640598f961016ca7ee
Leonardo Merza <ljmerza@gmail.com>
2023-07-27 13:35:33 -0400

    fix repolink parser

--- custom_components/email/parsers/reolink.py
+++ custom_components/email/parsers/reolink.py
@@ -16,9 +16,15 @@ def parse_reolink(email):
 
     soup = BeautifulSoup(email[EMAIL_ATTR_BODY], 'html.parser')
     links = [link.get('href') for link in soup.find_all('a')]
+
     for link in links:
         if not link:
             continue
+
+        match = re.search('trackNums=(.*?)$', link)
+        if match and match.group(1) not in tracking_numbers:
+            tracking_numbers.append(match.group(1))
+
         match = re.search('qtc_tLabels1=(.*?)$', link)
         if match and match.group(1) not in tracking_numbers:
             tracking_numbers.append(match.group(1))
